22e6ec12824adb4e0babc5f41e5ccf6ff9694caa
James Ekow Abaka Ainooson <jainooson@gmail.com>
2011-04-12 20:32:36 +0000

    More more more refactoring ... agian. Moved the widgets back into the views. They really didn't belong to controllers found a more streamlined approach to access them from the views
    git-svn-id: https://ntentan.googlecode.com/svn/trunk@139 05a48904-4b55-0410-ac7d-d94522421892

---
+++ widgets/Widget.php
@@ -0,0 +1,125 @@
+<?php
+/* 
+ * Ntentan PHP Framework
+ * Copyright 2010 James Ekow Abaka Ainooson
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace ntentan\views\widgets;
+
+use ntentan\views\template_engines\Template;
+use ntentan\Ntentan;
+use ntentan\views\Presentation;
+use ntentan\caching\Cache;
+
+/**
+ * Enter description here ...
+ * @author ekow
+ * @todo Look at the possibility of renaming blocks to widgets
+ */
+class Widget extends Presentation
+{
+    protected $data = array();
+    protected $template;
+    protected $name;
+    protected $alias;
+    private $filePath;
+    public $cacheLifetime = -1;
+
+    public function __construct($args)
+    {
+        
+    }
+
+    public function init()
+    {
+        
+    }
+    
+    public function setFilePath($filePath)
+    {
+        $this->filePath = $filePath;
+    }
+
+    public function getName() {
+    	return $this->name;
+    }
+    
+    public function setName($name) {
+    	$this->name = $name;
+    }
+
+    public function setAlias($alias)
+    {
+        $this->alias = $alias;
+    }
+
+    protected function set($params1, $params2 = null) {
+    	
+        if(is_array($params1)) {
+            $this->data = array_merge($this->data, $params1);
+        } else {
+            $this->data[$params1] = $params2;
+        }
+    }
+
+    protected function getData()
+    {
+        return $this->data;
+    }
+
+    public function preRender()
+    {
+
+    }
+
+    public function postRender()
+    {
+        
+    }
+
+    public function __toString()
+    {
+        $cacheKey = $this->getCacheKey();
+        if(Cache::exists($cacheKey))
+        {
+            $output = Cache::get($cacheKey);
+        }
+        else
+        {
+            $this->preRender();
+            if($this->template == "")
+            {
+                $widget = $this->getName();
+                $this->template = $this->filePath . "/$widget.tpl.php";
+            }
+            $this->set('alias', $this->alias);
+            $output = Template::out($this->template, $this->data);
+
+            $this->postRender();
+            Cache::add($cacheKey, $output, $this->cacheLifetime);
+        }
+        return $output;
+    }
+
+    private function getCacheKey()
+    {
+        return $this->alias . '_' . $this->getName() . '_widget';
+    }
+
+    public function cached()
+    {
+        return Cache::exists($this->getCacheKey());
+    }
+}
