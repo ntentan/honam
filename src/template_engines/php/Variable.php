88516a266206f62c3de806388e872a0eed4d4317
Ekow Abaka Ainooson <jainooson@gmail.com>
2015-08-18 20:00:55 +0000

    Fixed bug with notices thrown by variable class for non existent offsets

--- src/template_engines/php/Variable.php
+++ src/template_engines/php/Variable.php
@@ -1,111 +1,113 @@
 <?php
+
 namespace ntentan\honam\template_engines\php;
 
 class Variable implements \ArrayAccess, \Iterator, \Countable
 {
+
     private $keys;
     private $position;
     private $data;
-    
+
     public static function initialize($data)
     {
         $type = gettype($data);
-        switch ($type)
-        {
+        switch ($type) {
+            case 'NULL':
             case 'string':
                 return new Variable($data);
-                
+
             case 'array':
                 return new Variable($data, array_keys($data));
-                
+
             case 'boolean':
             case 'integer':
             case 'double':
-            case 'NULL':
             case 'object':
                 return $data;
-                
+
             default:
                 throw new \ntentan\honam\ViewException("Cannot handle the $type type in templates");
         }
     }
-
 
     public function __construct($data, $keys = array())
     {
         $this->data = $data;
         $this->keys = $keys;
     }
-    
+
     public function __toString()
     {
         return Janitor::cleanHtml($this->data);
     }
-    
+
     public function u()
     {
         return $this->unescape();
     }
-    
+
     public function count()
     {
         return count($this->data);
     }
-    
+
     public function unescape()
     {
         return $this->data;
     }
-    
-    public function rewind() 
+
+    public function rewind()
     {
         return $this->position = 0;
     }
 
-    public function valid() 
+    public function valid()
     {
         return @isset($this->data[$this->keys[$this->position]]);
-    }    
+    }
 
-    public function current() 
+    public function current()
     {
         return Variable::initialize($this->data[$this->keys[$this->position]]);
     }
 
-    public function key() 
+    public function key()
     {
         return $this->keys[$this->position];
     }
 
-    public function next() 
+    public function next()
     {
         $this->position++;
     }
 
-    public function offsetExists($offset) 
+    public function offsetExists($offset)
     {
         return isset($this->data[$offset]);
     }
 
-    public function offsetGet($offset) 
+    public function offsetGet($offset)
     {
-        return Variable::initialize($this->data[$offset]);
+        if (isset($this->data[$offset])) {
+            return Variable::initialize($this->data[$offset]);
+        } else {
+            return Variable::initialize(null);
+        }
     }
 
-    public function offsetSet($offset, $value) 
+    public function offsetSet($offset, $value)
     {
-        if(is_null($offset))
-        {
+        if (is_null($offset)) {
             $this->data[] = $value;
-        }
-        else
-        {
+        } else {
             $this->data[$offset] = $value;
         }
     }
 
-    public function offsetUnset($offset) 
+    public function offsetUnset($offset)
     {
         unset($this->data[$offset]);
     }
+
 }
