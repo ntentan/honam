57815fd015da4405c190797cd22631eb8b1edab0
Ekow Abaka Ainooson <jainooson@gmail.com>
2015-11-09 00:14:15 +0000

    Fixing escaping in certain form templates

--- src/template_engines/php/functions.php
+++ src/template_engines/php/functions.php
@@ -16,3 +16,12 @@ function t($template, $templateData = array())
 {
     return TemplateEngine::render($template, $templateData);
 }
+
+function unescape($item)
+{
+    if($item instanceof ntentan\honam\template_engines\php\Variable) {
+        return $item->unescape();
+    } else {
+        return $item;
+    }
+}
--- templates/forms/select_element.tpl.php
+++ templates/forms/select_element.tpl.php
@@ -1,7 +1,7 @@
 <select <?= t("element_attributes.tpl.php", array('attributes' => $element->getAttributes())) ?> >
 
 <?php $options = $element->getOptions();
-$selectValue = $element->getValue();
+$selectValue = $element->getValue()->unescape();
 foreach($options->unescape() as $value => $label):?>
 <option value='<?= $value ?>' <?=($selectValue == $value ? "selected='selected'":"") ?> ><?= $label ?></option>
 <?php endforeach; ?>
