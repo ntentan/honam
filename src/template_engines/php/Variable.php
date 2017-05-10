214a3a6e3ac7d75264eea7f78a94ceb46b50b71d
James Ainooson <jainooson@gmail.com>
2017-05-10 17:21:08 -0500

    A little formatting here and there

--- src/template_engines/php/Variable.php
+++ src/template_engines/php/Variable.php
@@ -2,15 +2,14 @@
 
 namespace ntentan\honam\template_engines\php;
 
-class Variable implements \ArrayAccess, \Iterator, \Countable
-{
+class Variable implements \ArrayAccess, \Iterator, \Countable {
+
     private $keys;
     private $position;
     private $data;
     private $iteratable = false;
 
-    public static function initialize($data)
-    {
+    public static function initialize($data) {
         $type = gettype($data);
         switch ($type) {
             case 'object':
@@ -31,95 +30,81 @@ class Variable implements \ArrayAccess, \Iterator, \Countable
         }
     }
 
-    public function __construct($data, $keys = array())
-    {
-        if($data instanceof Variable) {
+    public function __construct($data, $keys = array()) {
+        if ($data instanceof Variable) {
             $this->data = $data->getData();
             $this->keys = $data->getKeys();
         } else {
             $this->data = $data;
             $this->keys = $keys;
         }
-        
+
         if ($this->data instanceof \Iterator) {
             $this->iteratable = true;
         }
     }
 
-    public function __toString()
-    {
+    public function __toString() {
         return Janitor::cleanHtml($this->data);
     }
 
-    public function u()
-    {
+    public function u() {
         return $this->unescape();
     }
 
-    public function count()
-    {
+    public function count() {
         return count($this->data);
     }
 
-    public function unescape()
-    {
+    public function unescape() {
         return $this->data;
     }
 
-    public function rewind()
-    {
-        if($this->iteratable) {
+    public function rewind() {
+        if ($this->iteratable) {
             $this->data->rewind();
         } else {
             $this->position = 0;
         }
     }
 
-    public function valid()
-    {
-        if($this->iteratable) {
+    public function valid() {
+        if ($this->iteratable) {
             return $this->data->valid();
         } else {
             return @isset($this->data[$this->keys[$this->position]]);
         }
     }
 
-    public function current()
-    {
-        if($this->iteratable) {
+    public function current() {
+        if ($this->iteratable) {
             return $this->data->current();
         } else {
             return Variable::initialize($this->data[$this->keys[$this->position]]);
         }
     }
 
-    public function key()
-    {
-        if($this->iteratable)
-        {
+    public function key() {
+        if ($this->iteratable) {
             return $this->data->key();
         } else {
             return $this->keys[$this->position];
         }
     }
 
-    public function next()
-    {
-        if($this->iteratable)
-        {
+    public function next() {
+        if ($this->iteratable) {
             $this->data->next();
         } else {
             $this->position++;
         }
     }
 
-    public function offsetExists($offset)
-    {
+    public function offsetExists($offset) {
         return isset($this->data[$offset]);
     }
 
-    public function offsetGet($offset)
-    {
+    public function offsetGet($offset) {
         if (isset($this->data[$offset])) {
             return Variable::initialize($this->data[$offset]);
         } else {
@@ -127,8 +112,7 @@ class Variable implements \ArrayAccess, \Iterator, \Countable
         }
     }
 
-    public function offsetSet($offset, $value)
-    {
+    public function offsetSet($offset, $value) {
         if (is_null($offset)) {
             $this->data[] = $value;
         } else {
@@ -136,28 +120,24 @@ class Variable implements \ArrayAccess, \Iterator, \Countable
         }
     }
 
-    public function offsetUnset($offset)
-    {
+    public function offsetUnset($offset) {
         unset($this->data[$offset]);
     }
 
-    public function __call($method, $arguments)
-    {
+    public function __call($method, $arguments) {
         return Variable::initialize(call_user_func_array([$this->data, $method], $arguments));
     }
 
-    public function __get($name)
-    {
+    public function __get($name) {
         return Variable::initialize($this->data->$name);
     }
 
-    public function getData()
-    {
+    public function getData() {
         return $this->data;
     }
 
-    public function getKeys()
-    {
+    public function getKeys() {
         return $this->keys;
     }
+
 }
