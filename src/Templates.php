6b3af88e98a0c18749c982b990be0452d3d25e03
Ekow Abaka Ainooson <jainooson@gmail.com>
2020-02-26 00:14:35 -0600

    Templates utilities converted to double as a facade when running stand-alone

--- src/Templates.php
+++ src/Templates.php
@@ -6,21 +6,33 @@ namespace ntentan\honam;
 
 use ntentan\honam\TemplateFileResolver;
 use ntentan\honam\TemplateRenderer;
+use ntentan\honam\engines\PhpEngine;
+use ntentan\honam\engines\MustacheEngine;
 
 class Templates
 {
     private $templateRenderer;
     private $templateFileResolver;
 
-    public function __construct(TemplateFileResolver $templateFileResolver, TemplateRenderer $templateRenderer)
+    public function __construct(TemplateFileResolver $templateFileResolver = null, TemplateRenderer $templateRenderer = null)
     {
-        $this->templateFileResolver = $templateFileResolver;
-        $this->templateRenderer = $templateRenderer;
+        $engineRegistry = null;
+        $this->templateFileResolver = $templateFileResolver ?? new TemplateFileResolver();
+        $this->templateRenderer = $templateRenderer ?? new TemplateRenderer($engineRegistry = new EngineRegistry(), $templateFileResolver);
+        if($engineRegistry) {
+            $engineRegistry->registerEngine('mustache', new MustacheEngine());
+            $engineRegistry->registerEngine('php.tpl', new PhpEngine());
+        }
     }
 
     public function prependPath(string $path)
     {
         $this->templateFileResolver->prependToPathHierarchy($path);
+    }
+
+    public function appendPath(string $path)
+    {
+        $this->templateFileResolver->appendPath($path);
     }
 
     public function render(string $template, array $data)
