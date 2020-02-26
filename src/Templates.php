2e8b22b1d2bca775e2182b38073e7a6e310dcb0d
Ekow Abaka Ainooson <jainooson@gmail.com>
2020-02-26 12:18:30 -0600

    Facade is properly activating

--- src/Templates.php
+++ src/Templates.php
@@ -6,22 +6,25 @@ namespace ntentan\honam;
 
 use ntentan\honam\TemplateFileResolver;
 use ntentan\honam\TemplateRenderer;
-use ntentan\honam\engines\PhpEngine;
-use ntentan\honam\engines\MustacheEngine;
+use ntentan\honam\factories\PhpEngineFactory;
+use ntentan\honam\factories\MustacheEngineFactory;
+use ntentan\honam\engines\php\HelperVariable;
+use ntentan\honam\engines\php\Janitor;
 
 class Templates
 {
     private $templateRenderer;
     private $templateFileResolver;
+    private $engineRegistry;
 
     public function __construct(TemplateFileResolver $templateFileResolver = null, TemplateRenderer $templateRenderer = null)
     {
-        $engineRegistry = null;
         $this->templateFileResolver = $templateFileResolver ?? new TemplateFileResolver();
-        $this->templateRenderer = $templateRenderer ?? new TemplateRenderer($engineRegistry = new EngineRegistry(), $templateFileResolver);
-        if($engineRegistry) {
-            $engineRegistry->registerEngine('mustache', new MustacheEngine());
-            $engineRegistry->registerEngine('php.tpl', new PhpEngine());
+        $this->templateRenderer = $templateRenderer ?? new TemplateRenderer($this->engineRegistry = new EngineRegistry(), $this->templateFileResolver);
+        if($this->engineRegistry) {
+            $this->engineRegistry->registerEngine(['.mustache'], new MustacheEngineFactory($this->templateFileResolver));
+            $helperVariable = new HelperVariable($this->templateRenderer, $this->templateFileResolver);
+            $this->engineRegistry->registerEngine(['.tpl.php'], new PhpEngineFactory($this->templateRenderer, $helperVariable, new Janitor()));
         }
     }
 
