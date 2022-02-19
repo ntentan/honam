3b40ced70f45a3a504e60e1bb6940a431d54eb00
Ekow Abaka Ainooson <jainooson@gmail.com>
2022-02-18 22:23:32 -0600

    Updating the menu helper

--- templates/menu/menu.tpl.php
+++ templates/menu/menu.tpl.php
@@ -1,14 +1,12 @@
 <?php if(count($items) > 0):?>
     <?php $css_classes = $css_classes->unescape() ?>
-    <ul class='menu <?= implode(' ', $css_classes['menu'] ?? []) ?>' <?php if($alias != ''): ?>id="<?= $alias ?>-menu"<?php endif; ?>><?php
-        foreach($items as $item){
+    <ul class='<?= implode(' ', $css_classes['menu'] ?? []) ?>' <?php if($alias != ''): ?>id="<?= $alias ?>-menu"<?php endif; ?>>
+        <?php foreach($items as $item){
             $params = '';
-            foreach($item as $key => $value)
-            {
+            foreach($item as $key => $value) {
                 if($key == 'url' || $key == 'label' || $key == 'selected' || $key == 'id' || $key == 'fully_matched' || $key == 'children') continue;
                 $params .= "$key = '$value' ";
             }
-            $id = isset($item['id']) ? $item['id'] : preg_replace('~-+~', '-', 'menu-item-' . str_replace("/","-",strtolower($item["url"])));
             echo $this->partial( "{$alias}_menu_item", ['params' => $params, 'id' => $id, 'item' => $item, 'has_links' => $has_links ?? false, 'css_classes' => $css_classes]) ;
         } ?></ul>
 <?php endif ?>
